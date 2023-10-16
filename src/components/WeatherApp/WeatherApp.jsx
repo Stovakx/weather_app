864dbc92841a58c941c16deb762e0904ecd9476c
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-10-16 22:55:29 +0200

    error message if city doesn't exist

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,44 +1,48 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './WeatherApp.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./WeatherApp.css";
 
 export default function WeatherApp() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('');
-  const [tempUnits, setTempUnits] = useState('F');
+  const [location, setLocation] = useState("");
+  const [tempUnits, setTempUnits] = useState("F");
+  const [errorMessage, setErrorMessage ] = useState('');
 
   // Přepínání mezi imperial a metric
   const toggleTempUnit = () => {
-    const newTempUnits = tempUnits === 'F' ? 'C' : 'F';
+    const newTempUnits = tempUnits === "F" ? "C" : "F";
     setTempUnits(newTempUnits);
     searchLocation(newTempUnits); // Aktualizace po změně jednotek
   };
 
   const searchLocation = (newTempUnits = tempUnits) => {
-    if (location.trim() === '') {
+    if (location.trim() === "") {
       return;
     }
 
-    const unitsParam = newTempUnits === 'F' ? 'imperial' : 'metric';
+    const unitsParam = newTempUnits === "F" ? "imperial" : "metric";
     const updatedWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unitsParam}&appid=ab9e85a1335be4935928ed2dc11c9a55`;
 
     axios.get(updatedWeatherURL).then((response) => {
       setData(response.data);
+      setErrorMessage('');
+    })
+    .catch(()=>{
+      setErrorMessage('Město nebylo nalezeno v databázi.')
     });
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       searchLocation();
     }
   };
 
   useEffect(() => {
-    if (location && location.trim() !== '' && location.endsWith('\n')) {
+    if (location && location.trim() !== "" && location.endsWith("\n")) {
       searchLocation();
     }
   }, [location]);
-
 
   return (
     <div className="card">
@@ -50,37 +54,48 @@ export default function WeatherApp() {
           placeholder="Zadejte lokaci"
         />
         <button onClick={toggleTempUnit}>Změna jednotek</button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
       </div>
-      <div className='header'>
-        <div className='location'>
+      <div className="header">
+        <div className="location">
           <p>{data.name}</p>
         </div>
-        <div className='temp'>
-          {data.main ? <h1>{tempUnits === 'F' ? `${data.main.temp.toFixed()}°F` : `${data.main.temp.toFixed()}°C`}</h1> : null}
+        <div className="temp">
+          {data.main ? (
+            <h1>
+              {tempUnits === "F"
+                ? `${data.main.temp.toFixed()}°F`
+                : `${data.main.temp.toFixed()}°C`}
+            </h1>
+          ) : null}
         </div>
         {data.weather ? (
-        <div className='description'>
-          <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png` } alt="weather icon" /> <p> {data.weather[0].description}</p>
-        </div>
+          <div className="description">
+            <img
+              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+              alt="weather icon"
+            />{" "}
+            <p> {data.weather[0].description}</p>
+          </div>
         ) : null}
       </div>
       {data.main && data.weather ? (
-        <div className='bottom'>
-          <div className='feelsTemp'>
+        <div className="bottom">
+          <div className="feelsTemp">
             <p>
-              {tempUnits === 'F'
+              {tempUnits === "F"
                 ? `${data.main.feels_like.toFixed()}°F`
                 : `${data.main.feels_like.toFixed()}°C`}
             </p>
             <p>Pocitová teplota</p>
           </div>
-          <div className='Humidity'>
+          <div className="Humidity">
             <p>{data.main.humidity}%</p>
             <p>Vlhkost</p>
           </div>
-          <div className='wind'>
+          <div className="wind">
             <p>
-              {tempUnits === 'F'
+              {tempUnits === "F"
                 ? `${data.wind.speed} MPH`
                 : `${data.wind.speed} m/s`}
             </p>
@@ -89,5 +104,5 @@ export default function WeatherApp() {
         </div>
       ) : null}
     </div>
-  )
+  );
 }
